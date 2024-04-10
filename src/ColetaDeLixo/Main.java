163f5b1eedd80f4de52d0672af5c3f2971c3442d
Caio-T4vares <caio.hosana@hotmail.com>
2024-04-09 22:15:38 -0300

    fahasEulerPath e duplicateEdges

--- src/ColetaDeLixo/Main.java
+++ src/ColetaDeLixo/Main.java
@@ -2,7 +2,6 @@ package ColetaDeLixo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Stack;
 
 public class Main {
  
@@ -12,24 +11,35 @@ public class Main {
     // se não, então acha o menor caminho que passe por todos os vértices impares usando djkistra e em seguida gera o supergrafo G*
     // duplicando as arestas do caminho. Aí em seguida, usa o algoritmo de Hierholzer.
     Vertice[] vertices = new Vertice[6];
-    vertices[0] = new Vertice("S");
-    vertices[1] = new Vertice("A");
-    vertices[2] = new Vertice("B");
-    vertices[3] = new Vertice("C");
-    vertices[4] = new Vertice("D");
-    vertices[5] = new Vertice("T");
+    vertices[0] = new Vertice("A");
+    vertices[1] = new Vertice("B");
+    vertices[2] = new Vertice("C");
+    vertices[3] = new Vertice("D");
+    vertices[4] = new Vertice("E");
+    vertices[5] = new Vertice("F");
     ArrayList<Aresta> arestas = new ArrayList<>();
-    arestas.add(new Aresta(vertices[0],vertices[1], 18)); // S - A
-    arestas.add(new Aresta(vertices[0],vertices[3], 15)); // S - C
-    arestas.add(new Aresta(vertices[1],vertices[3], 2));  // A - C
-    arestas.add(new Aresta(vertices[2],vertices[1], 9));  // B - A
-    arestas.add(new Aresta(vertices[3],vertices[2], 14)); // C - B
-    arestas.add(new Aresta(vertices[3],vertices[4], 7)); // C - D
-    arestas.add(new Aresta(vertices[4],vertices[2], 10)); // D - B
-    arestas.add(new Aresta(vertices[5],vertices[2], 28)); // T - B
-    arestas.add(new Aresta(vertices[4],vertices[5], 36)); // D - T
+    arestas.add(new Aresta(vertices[0],vertices[1], 5)); // A - B
+    arestas.add(new Aresta(vertices[0],vertices[3], 2)); // A - C
+    arestas.add(new Aresta(vertices[1],vertices[3], 7));  // C - B
+    arestas.add(new Aresta(vertices[2],vertices[1], 8));  // C - E
+    arestas.add(new Aresta(vertices[3],vertices[2], 8)); // B - D
+    arestas.add(new Aresta(vertices[3],vertices[4], 3)); //  E - F
+    arestas.add(new Aresta(vertices[4],vertices[2], 6)); // E - D
+    arestas.add(new Aresta(vertices[5],vertices[2], 4)); // C - D
+    arestas.add(new Aresta(vertices[4],vertices[5], 4)); // F - D
     Graph graph = new Graph(vertices, arestas);
-    dijkistra(graph, graph.getVertices()[0]); // usando S de origem
+    if(hasEulerTour(graph)){
+      System.out.println("É euleriano");
+    } else {
+      System.out.println("Não é euleriano");
+      Graph minimumCostPath = dijkistra(graph, graph.getVertices()[0]); // usando A de origem
+      duplicateEdges(graph, minimumCostPath);
+      if(hasEulerTour(graph))
+          System.out.println("Funfa");
+      else
+          System.out.println("N funfa");
+      hierholzer(graph);
+    }
   }
 
 
@@ -56,28 +66,22 @@ public class Main {
       for(Aresta a: graph.getEdges()){
         if(a.getV1().getName().equals(removedName) && lambdas.containsKey(a.getV2())){
           // se entrar, o vizinho é v2 e o removido foi v1
-          System.out.println("Removido : " + removedName);
-          System.out.println("Aresta : " + a.getV1().getName() + " -> " + a.getV2().getName());
           int lmbdaAtualizado = Math.min(lambdas.get(a.getV2()), removedCost + a.getWeight());
           if(lmbdaAtualizado < lambdas.get(a.getV2())){ // atualiza o lmbda
             lambdas.put(a.getV2(), lmbdaAtualizado);
-            System.out.println("Lmbda que foi att: " + lmbdaAtualizado);
             // att predecessores
             predecessors.put(a.getV2(), removedVertice);
           }
             
         } else if(a.getV2().getName().equals(removedName) && lambdas.containsKey(a.getV1())) {
           // se entrar, o vizinho é v1 e o removido foi v2
-          System.out.println("Removido : " + removedName);
-          System.out.println("Aresta : " + a.getV1().getName() + " -> " + a.getV2().getName());
+          
           int lmbdaAtualizado = Math.min(lambdas.get(a.getV1()), removedCost + a.getWeight());
           if (lmbdaAtualizado < lambdas.get(a.getV1())) {
             lambdas.put(a.getV1(), lmbdaAtualizado);
             // att predecessores
-            System.out.println("Lmbda que foi att: " + lmbdaAtualizado);
             predecessors.put(a.getV1(), removedVertice);
           }
-          System.out.println("");
         }
       }
     }
@@ -92,28 +96,43 @@ public class Main {
       arestas.add(new Aresta(v, predecessors.get(v), 0));
       cont++;
     }
-    for(Vertice v: predecessors.keySet()){
-      System.out.println(v.getName() + " tem como predecessor : " + predecessors.get(v).getName());
-    }
     Graph minimumCostPath = new Graph(vertices, arestas);
 
-    for(Aresta a: minimumCostPath.getEdges()){
-      System.out.println("Aresta: " + a.getV1().getName() +  " => " + a.getV2().getName());
-    }
-    for(Vertice v: minimumCostPath.getVertices()){
-      System.out.println("Vertice: " + v.getName());
-    }
-
-    return null;
+    return minimumCostPath;
   }
   public static boolean hasEulerTour(Graph graph){
-    return false;
+    // verificar se há algum vertice com grau impar 
+    int impares = 0;
+    for(Vertice v: graph.getVertices()){
+      int grau = 0;
+      for(Aresta a: graph.getEdges()){
+        if(a.getV1().getName().equals(v.getName()) || a.getV2().getName().equals(v.getName())){
+          // se o vertice participar de uma aresta
+          grau++;
+        }
+      }
+      if(grau % 2 != 0)
+        impares++;
+    }
+    if(impares == 2)
+      return false;
+    if(impares == 0)
+      return true;
+
+    return false; // se tiver mais do que 2 impares, o algoritmo já não funfa
   }
   public static Graph hierholzer(Graph graph){
     return null;
   }
-  public static Graph duplicateEdges(Graph graph){
-    return null;
+  public static void duplicateEdges(Graph graph, Graph minimumCostPath){
+    for(Aresta a: graph.getEdges()){
+      for(Aresta b: minimumCostPath.getEdges()){
+        if(a.getV1().getName().equals(b.getV1().getName()) && a.getV2().getName().equals(b.getV2().getName()) ){
+          // verifica se é a mesma aresta para em seguida duplicar
+          graph.getEdges().add(new Aresta(a.getV1(), a.getV2(), a.getWeight()));
+        }
+      }
+    }
   }
   public static void inicializeLambdas(HashMap<Vertice, Integer> lambdas, Graph graph, Vertice root){
     for (Vertice v : graph.getVertices()) {
